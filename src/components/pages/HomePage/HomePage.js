b90e8a723beb381f6a73be45d68afa475bc3be25
Tubagus <dutapanggulu@gmail.com>
2020-05-17 15:24:51 +0700

    add top navbar

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -2,33 +2,82 @@ import React from 'react';
 import './HomePage.css';
 import TopNav from '../../atoms/TopNav/TopNav';
 import Input from '../../atoms/Input/Input';
-import { IoMdNotificationsOutline, IoMdArrowDropdown, IoMdAdd, IoMdSearch } from 'react-icons/all';
+import {
+    IoMdNotificationsOutline,
+    IoMdArrowDropdown,
+    IoMdAdd,
+    IoMdSearch,
+    IoMdExit,
+    IoMdHelpCircle,
+    IoMdSettings
+} from 'react-icons/all';
 import Logo from '../../../assets/img/main_logo.svg';
 import Avatar from '../../../assets/img/avatar.svg';
+import List, { Item } from '../../atoms/List2/List';
+import Menu, { Content } from '../../atoms/Menu/Menu';
 
 const HomePage = () => {
-    
     return (
         <div id="home-page">
             <TopNav>
                 <div className="left-wrapper">
-                    <img src={ Logo } alt="Buku Kita" width="35"/>
-                        <span className="brand" style={{ marginLeft:"10px", fontWeight: "bolder", fontSize:"18px"}}> <span style={{ fontSize:"18px" }}>Buku</span> Kita</span>
-                    </div>
+                    <img src={Logo} alt="Buku Kita" width="35" />
+                    <span className="brand" style={{ marginLeft: '10px', fontWeight: 'bolder', fontSize: '18px' }}>
+                        <span style={{ fontSize: '18px' }}>Buku</span> Kita
+                    </span>
+                </div>
                 <div className="input-wrapper">
                     <Input placeholder="Cari di Buku Kita" variant="full-border" />
-                    <IoMdSearch className="search-icon" size={20}/>
+                    <IoMdSearch className="search-icon" size={20} />
                 </div>
-                <div className="list-group">
-                    <button className="item circle"><IoMdAdd size={24}/></button>
-                    <button className="item circle"><IoMdArrowDropdown size={24}/></button>
-                    <button className="item circle"><IoMdNotificationsOutline size={24}/></button>
-                    <button className="item rectangle avatar-wrapper"><span className="avatar"><img src={ Avatar } width="24px" alt="Profile"/></span>Profile</button>
-                </div>
+                <List className="user-menu">
+                    <Item effect variant="button circle">
+                        <IoMdAdd size={24} />
+                    </Item>
+                    <Item effect variant="button circle">
+                        <IoMdNotificationsOutline size={24} />
+                    </Item>
+                    <Menu delay={-1} className="user-account-menu">
+                        <Item effect variant="button circle">
+                            <IoMdArrowDropdown size={24} />
+                        </Item>
+                        <Content id="1" >
+                            <List>
+                                <Item variant="div rectangle">
+                                    <span className="avatar">
+                                        <img src={Avatar} width="30px" alt="Profile" />
+                                    </span>
+                                    <span>
+                                        <h4 style={{ marginBottom: '4px' }}>Username</h4>
+                                        <h5 style={{ color: 'var(--default-dark)', fontWeight: '400' }}>Lihat profil anda</h5>
+                                    </span>
+                                </Item>
+                                <hr style={{ width: '100%', border: '1.5px solid var(--divider)', margin: '.5rem' }} />
+                                <Item variant="div rectangle">
+                                    <span className="item-icon"><IoMdHelpCircle size={24} /></span>
+                                    Bantuan
+                                </Item>
+                                <Item variant="div rectangle">
+                                    <span className="item-icon"><IoMdSettings size={24} /></span>
+                                    Pengaturan
+                                </Item>
+                                <Item variant="div rectangle">
+                                    <span className="item-icon"><IoMdExit size={24} /></span>
+                                    Keluar
+                                </Item>
+                            </List>
+                        </Content>
+                    </Menu>
+                    <Item variant="button rectangle" className="avatar-wrapper">
+                        <span className="avatar">
+                            <img src={Avatar} width="24px" alt="Profile" />
+                        </span>
+                        Profile
+                    </Item>
+                </List>
             </TopNav>
         </div>
-    )
-
-}
+    );
+};
 
 export default HomePage;
