c68a662c0bc1f2f1a9429110203a48504cd101c9
Tubagus <dutapanggulu@gmail.com>
2020-05-18 10:03:30 +0700

    fix bug, typo, & cleaneup

--- src/components/atoms/List2/List.js
+++ src/components/atoms/List2/List.js
@@ -2,24 +2,21 @@ import React from 'react';
 import './List.css';
 import { useRef } from 'react';
 import { useState } from 'react';
-import { useEffect } from 'react';
-
 export const Item = (props) => {
     const { variant, className, children, effect } = props;
-    let types = variant.split(' ');
+    let types = variant ? variant.split(' ') : [];
     const itemRef = useRef(null);
     const [toggle, setToggle] = useState(true);
 
-    useEffect(() => console.log(toggle));
-
     return React.createElement(types[0] ? types[0] : 'div', {
         ...props,
+        effect: undefined,
         ref: itemRef,
         onClick: (e) => {
             setToggle(!toggle);
             if (props.onClick) props.onClick(e);
         },
-        className: `item${types[1] ? ' ' + types[1] : ''}${toggle ? ' active' : ''}${className ? ' ' + className : ''}${effect ? ' effect' : ''}`
+        className: `item${types[1] ? ' ' + types[1] : ' circle'}${toggle ? ' active' : ''}${className ? ' ' + className : ''}${effect ? ' effect' : ''}`
     }, children);
 };
 
