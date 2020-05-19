174e2f8d7500cbdbe5ebb7b040a6e88b92250e84
Tubagus <dutapanggulu@gmail.com>
2020-05-19 09:40:20 +0700

    added "link"

--- src/components/atoms/List2/List.js
+++ src/components/atoms/List2/List.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import './List.css';
 import { useRef } from 'react';
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './List.css';
+
 export const Item = (props) => {
-    const { variant, className, children, effect } = props;
+    const { variant, className, children, effect, to } = props;
     let types = variant ? variant.split(' ') : [];
     const itemRef = useRef(null);
-    const [toggle, setToggle] = useState(true);
 
-    return React.createElement(types[0] ? types[0] : 'div', {
+    let newClass = `item${types[1] ? ' ' + types[1] : ' circle'}${className ? ' ' + className : ''}${effect ? ' effect' : ''}`;
+
+    return to ? (
+        <Link className={newClass} to={to}> {children} </Link>
+    ) : React.createElement(types[0] ? types[0] : 'div', {
         ...props,
         effect: undefined,
         ref: itemRef,
-        onClick: (e) => {
-            setToggle(!toggle);
-            if (props.onClick) props.onClick(e);
-        },
-        className: `item${types[1] ? ' ' + types[1] : ' circle'}${toggle ? ' active' : ''}${className ? ' ' + className : ''}${effect ? ' effect' : ''}`
+        className: newClass
     }, children);
 };
 
