4978d29454b1414b958e0104623c7da0599632cc
Tubagus <dutapanggulu@gmail.com>
2020-05-19 23:52:04 +0700

    refactoring

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,16 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
 
-export const useMatchMedia = (callBack) => {
-    
+export const useMedia = (callBack) => {
     return useEffect(() => {
-
-         window.addEventListener('resize', () => {
+        window.addEventListener('resize', () => {
             callBack(document.documentElement.clientWidth)
-         });
-
+        });
         return window.removeEventListener('resize', () => {
             callBack(document.documentElement.clientWidth)
-         });
-         
-    },[callBack])
+        });
+    }, [callBack]);
+}
+
+export const useMatchMedia = () => {
+    let initialWidth = document.documentElement.clientWidth;
+    const [width, setWidth] = useState(initialWidth);
+    useMedia((size) => setWidth(size));
+    return width;
 }
