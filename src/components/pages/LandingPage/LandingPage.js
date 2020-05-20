828d460629018adf10021bb8ed82028f6f1ed908
Tubagus <dutapanggulu@gmail.com>
2020-05-20 08:54:34 +0700

    change url imae

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ReactComponent as Wp } from '../../../assets/img/undraw_book_lover_mkck.svg';
+import { ReactComponent as Wp } from '../../../assets/img/science_discovery.svg';
 import bg from '../../../assets/img/bg.svg';
 import TopNav from '../../atoms/TopNav/TopNav';
 import Logo from '../../../assets/img/main_logo.svg';
