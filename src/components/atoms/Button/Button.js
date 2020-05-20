eabe669b30391047e6a29e1358808bc18248cbc2
Tubagus <dutapanggulu@gmail.com>
2020-05-20 08:56:10 +0700

    refactoring

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -3,19 +3,15 @@ import './Button.css';
 import { Link } from 'react-router-dom';
 
 const Button = (props) => {
-    
-    const { variant, disabled, onClick, children, to, className, onMouseOver, style } = props;
 
-    return ( 
-        variant === 'link' ? <Link style={style} to={ to } className="btn btn-link">{ children }</Link>
-                                        :
-                            <button
-                                className={`btn${ variant ? ' ' + variant: '' }${ className ? ' ' + className: '' }`}
-                                onClick={ onClick }
-                                disabled={ disabled }
-                                onMouseOver={ onMouseOver }
-                                style={ style }
-                            >{ children }</button>
+    const { variant, children, to, className, style } = props;
+
+    return (
+        variant === 'link' ? <Link style={style} to={to} className="btn btn-link">{children}</Link> :
+            React.createElement('button', {
+                ...props,
+                className: `btn${variant ? ' ' + variant : ''}${className ? ' ' + className : ''}`
+            }, children)
     )
 }
 
