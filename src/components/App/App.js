43d29b61776b2ef6f76857d022f2b9696125fd4d
Tubagus <dutapanggulu@gmail.com>
2020-05-22 02:20:32 +0700

    cleaned up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,17 @@ import LandingPage from '../pages/LandingPage/LandingPage';
 import Components from '../pages/Components/Components';
 import RegisterPage from '../pages/RegisterPage/RegisterPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
-import './App.css';
 import Base from '../pages/Base/Base';
+import Payment from '../pages/PaymentPage/Payment';
+import './App.css';
 
 const App = () => {
     return (
         <Router>
             <Switch>
+                <Route path="/payment">
+                    <Payment />
+                </Route>
                 <Route path="/home">
                     <Base />
                 </Route>
